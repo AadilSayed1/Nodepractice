9489b4f2b7f2f12cb1ac3c805a3347246b3da945
Aadil Sayed <aadilsayed19@gmail.com>
2022-10-06 14:41:52 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,8 @@
+// Node.js program to display some
+// text on consle screen 
+ 
+// Accessing console module 
+const console = require('console');
+ 
+// Calling console.info() method 
+console.info("Hello World");
